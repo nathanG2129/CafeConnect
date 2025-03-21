743b2fee669ecaca5893fa0d2b8d71ccbbcc06ce
Nathan Gaya <nathanpaul212907@gmail.com>
2025-03-21 12:27:30 +0800

    Add UserModel class and integrate user details in RegistrationPage
    - Created UserModel to encapsulate user information such as name, email, phone number, favorite coffee, preferred visit time, and special preferences.
    - Updated RegistrationPage to utilize UserModel for managing user details, including adding a user details card and functionality for editing and deleting user information.
    - Enhanced form validation and input handling for user registration, ensuring a better user experience.

---
+++ lib/models/userModel.dart
@@ -0,0 +1,17 @@
+class UserModel {
+  String? name;
+  String? email;
+  String? phoneNumber;
+  String? favoriteCoffee;
+  String? preferredVisitTime;
+  String? specialPreferences;
+
+  UserModel({
+    this.name,
+    this.email,
+    this.phoneNumber,
+    this.favoriteCoffee,
+    this.preferredVisitTime,
+    this.specialPreferences,
+  });
+}
