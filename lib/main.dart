1cec5ab34a6977bad7653dd87cafb19e809f0288
ljpb06 <leonardjericho.balang.cics@ust.edu.ph>
2025-03-28 11:59:23 +0800

    Added Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await GetStorage.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
